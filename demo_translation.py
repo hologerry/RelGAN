cdcb1b4326665cdf55858461d078301ce3fdac33
Gerry Gao <hologerry@gmail.com>
2020-07-02 18:00:16 +0800

    update test script

--- demo_translation.py
+++ demo_translation.py
@@ -10,14 +10,14 @@ import os
 
 import numpy as np
 import tensorflow as tf
-from keras import backend as K
+# from keras import backend as K
 # from keras.models import load_model
 from keras.backend.tensorflow_backend import set_session
 from keras.layers import Input
 from keras.models import Model
 from PIL import Image
 from skimage import io, transform
-from tqdm import tqdm
+# from tqdm import tqdm
 
 # from contrib.ops import SwitchNormalization
 from module import generator
@@ -46,6 +46,11 @@ def tileAttr2(x):
 # train_path = '/share/diskB/willy/GanExample/FaceAttributeChange_StarGAN/model/generator499.h5'
 
 
+def att2vec(attt):
+    temp_vec = np.expand_dims(attt, axis=0)
+    return temp_vec
+
+
 def testPic(img, gender, bangs=-1, glasses=1):
     temp3 = io.imread(img)
     tempb = transform.resize(temp3, [256, 256])
@@ -57,10 +62,6 @@ def testPic(img, gender, bangs=-1, glasses=1):
 
     new_attrs = ['5_o_Clock_Shadow', 'Bald', 'Bangs', 'Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Eyeglasses', 'Goatee',
                  'Gray_Hair', 'Male', 'Mustache', 'Pale_Skin', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Hat', 'Young']
-
-    def att2vec(attt):
-        temp_vec = np.expand_dims(attt, axis=0)
-        return temp_vec
 
     attrs_pos = []
     # attrs_neg = []
@@ -138,11 +139,87 @@ def testPic(img, gender, bangs=-1, glasses=1):
     return ans
 
 
+selected_attrs = ['5_o_Clock_Shadow', 'Bald', 'Bangs', 'Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Eyeglasses', 'Goatee',
+                  'Gray_Hair', 'Male', 'Mustache', 'Pale_Skin', 'Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Hat', 'Young']
+
+
+def test_one_img_hair(img_path, attr, test_idx=0):
+    temp3 = io.imread(img_path)
+    image = transform.resize(temp3, [256, 256])
+    image = image[:, :, :3]
+    image = image*2 - 1
+    one_image = np.expand_dims(image, axis=0)
+    new_image_batch = np.tile(one_image, [4, 1, 1, 1])  # 4 is hair color numbers
+    attrs = []
+    hair_colors = ['Black_Hair', 'Blond_Hair', 'Brown_Hair', 'Gray_Hair']
+    hair_color_idxes = []
+    for color in hair_colors:
+        hair_color_idxes.append(selected_attrs.index(color))
+    for color in hair_colors:
+        tmp_attr = np.zeros([17])
+        for idx in hair_color_idxes:
+            if attr[idx] == 1:
+                tmp_attr[idx] = -1
+        tmp_attr[selected_attrs.index(color)] = 1
+        tmp_attr = np.expand_dims(tmp_attr, axis=0)
+        attrs.append(tmp_attr)
+    new_attrs = np.concatenate(attrs, axis=0)
+    output_hair, _ = relGan.predict([new_image_batch, new_attrs])
+    save_images = np.concatenate([new_image_batch, output_hair], axis=0)
+    width = 1
+    height = 4 + 1
+    new_im = Image.new('RGB', (256*height, 256*width))
+    for h in range(height):
+        for w in range(width):
+            index = h*width+w
+            cur_image = (save_images[index]/2+0.5)*255
+            cur_image = cur_image.astype(np.uint8)
+            new_im.paste(Image.fromarray(cur_image, "RGB"), (256*h, 256*w))
+    # ans = np.array(new_im)
+    img_name = img_path.split('/')[-1].split('.')[0]
+    new_im.save(f'results_compare/test_hair_v{version:04d}_{test_idx}_{img_name}.jpg')
+
+
+def test_one_img_each_attr(img_path, attr, test_idx=0):
+    temp3 = io.imread(img_path)
+    image = transform.resize(temp3, [256, 256])
+    image = image[:, :, :3]
+    image = image*2 - 1
+    one_image = np.expand_dims(image, axis=0)
+    change_attr = ['Eyeglasses', 'Male', 'Mustache', 'Smiling', 'Young']
+    new_image_batch = np.tile(one_image, [len(change_attr), 1, 1, 1])  # 4 is hair color numbers
+    attrs = []
+    for change in change_attr:
+        tmp_attr = np.zeros([17])
+        idx = selected_attrs.index(change)
+        if attr[idx] == 1:
+            tmp_attr[idx] = -1
+        else:
+            tmp_attr[idx] = 1
+        tmp_attr = np.expand_dims(tmp_attr, axis=0)
+        attrs.append(tmp_attr)
+    new_attrs = np.concatenate(attrs, axis=0)
+    output_each, _ = relGan.predict([new_image_batch, new_attrs])
+    save_images = np.concatenate([new_image_batch, output_each], axis=0)
+    width = 1
+    height = len(change_attr) + 1
+    new_im = Image.new('RGB', (256*height, 256*width))
+    for h in range(height):
+        for w in range(width):
+            index = h*width+w
+            cur_image = (save_images[index]/2+0.5)*255
+            cur_image = cur_image.astype(np.uint8)
+            new_im.paste(Image.fromarray(cur_image, "RGB"), (256*h, 256*w))
+    # ans = np.array(new_im)
+    img_name = img_path.split('/')[-1].split('.')[0]
+    new_im.save(f'results_compare/test_each_attribute_v{version:04d}_{test_idx}_{img_name}.jpg')
+
+
 # temp3 = io.imread('/share/data/celeba-hq/celeba-256/12345.jpg')
 # version = int(sys.argv[2])
 # if version==-1:
 #     version = len(os.listdir('img'))-2
-version = 51
+version = 519
 
 print(version)
 
@@ -152,23 +229,23 @@ train_path = './generator'+str(version)+'.h5'
 # train_path = 'good_v0513.h5'
 
 
-def orthogonal(w):
+# def orthogonal(w):
 
-    w_kw = K.int_shape(w)[0]
-    w_kh = K.int_shape(w)[1]
-    # w_w = K.int_shape(w)[2]
-    # w_h = K.int_shape(w)[3]
+#     w_kw = K.int_shape(w)[0]
+#     w_kh = K.int_shape(w)[1]
+#     # w_w = K.int_shape(w)[2]
+#     # w_h = K.int_shape(w)[3]
 
-    temp = 0
-    for i in range(w_kw):
-        for j in range(w_kh):
-            wwt = tf.matmul(tf.transpose(w[i, j]), w[i, j])
-            mi = K.ones_like(wwt) - K.identity(wwt)
-            a = wwt * mi
-            a = tf.matmul(tf.transpose(a), a)
-            a = a * K.identity(a)
-            temp += K.sum(a)
-    return 1e-4 * temp
+#     temp = 0
+#     for i in range(w_kw):
+#         for j in range(w_kh):
+#             wwt = tf.matmul(tf.transpose(w[i, j]), w[i, j])
+#             mi = K.ones_like(wwt) - K.identity(wwt)
+#             a = wwt * mi
+#             a = tf.matmul(tf.transpose(a), a)
+#             a = a * K.identity(a)
+#             temp += K.sum(a)
+#     return 1e-4 * temp
 
 
 img_shape = (256, 256, 3)
@@ -184,30 +261,30 @@ relGan = Model(inputs=[imgA_input, vec_input_pos], outputs=g_out)
 relGan.load_weights(train_path)
 relGan.summary()
 
-lengh = 10
+
+celeba_test_imgs = open('celeba_test_img_list.txt', 'r').readlines()
+lengh = len(celeba_test_imgs)
 temp = [None]*lengh
-# celeba_test_imgs = open('celeba_test_img_list.txt', 'r').readlines()
-# for i in range(lengh):
-#     if i >= 10:
-#         gls = -1
-#     else:
-#         gls = 1
-#     temp[i] = testPic('test_img/' + celeba_test_imgs[i].strip(), i % 2, gls)
+for i in range(lengh):
+    img = os.path.join('test_img', 'celeba_test_'+celeba_test_imgs[i].split()[0])
+    attr = celeba_test_imgs[i].split()[1:]
+    test_one_img_hair(img, attr, i)
+    test_one_img_each_attr(img, attr, i)
 
-temp[0] = testPic('test_img/j.png', 0)
-temp[1] = testPic('test_img/c.2.jpg', 0)
-temp[2] = testPic('test_img/es.png', 1)
-temp[3] = testPic('test_img/e.2.png', 1)
-temp[4] = testPic('test_img/g.2.png', 1)
-temp[5] = testPic('test_img/y3.png', 1)
-temp[6] = testPic('test_img/f1.png', 1, glasses=-1)
-temp[7] = testPic('test_img/j1.png', 0, glasses=-1)
-temp[8] = testPic('test_img/c3.png', 0)
-temp[9] = testPic('test_img/g3.png', 1, glasses=-1)
+# temp[0] = testPic('test_img/j.png', 0)
+# temp[1] = testPic('test_img/c.2.jpg', 0)
+# temp[2] = testPic('test_img/es.png', 1)
+# temp[3] = testPic('test_img/e.2.png', 1)
+# temp[4] = testPic('test_img/g.2.png', 1)
+# temp[5] = testPic('test_img/y3.png', 1)
+# temp[6] = testPic('test_img/f1.png', 1, glasses=-1)
+# temp[7] = testPic('test_img/j1.png', 0, glasses=-1)
+# temp[8] = testPic('test_img/c3.png', 0)
+# temp[9] = testPic('test_img/g3.png', 1, glasses=-1)
 
-new_im = Image.new('RGB', (256*10, 256*lengh))
-for jj in tqdm(range(lengh)):
-    index = jj
-    image = temp[index]
-    new_im.paste(Image.fromarray(image, "RGB"), (0, 256*jj))
-new_im.save('results/test_v%04d.jpg' % version)
+# new_im = Image.new('RGB', (256*10, 256*lengh))
+# for jj in tqdm(range(lengh)):
+#     index = jj
+#     image = temp[index]
+#     new_im.paste(Image.fromarray(image, "RGB"), (0, 256*jj))
+# new_im.save('results/test_v%04d.jpg' % version)
